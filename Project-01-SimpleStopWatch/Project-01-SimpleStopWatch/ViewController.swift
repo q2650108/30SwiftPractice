756066eb3c8d95b33f59a021f7635d8788454dce
SHUN <SHUN@shundeimac.calcomp.com.tw>
2016-10-07 11:01:38 +0800

    add size classes  -  let screen can Portrait and Landscape

--- Project-01-SimpleStopWatch/Project-01-SimpleStopWatch/ViewController.swift
+++ Project-01-SimpleStopWatch/Project-01-SimpleStopWatch/ViewController.swift
@@ -33,12 +33,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        initUI()
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        initUI()
+        print("viewDidAppear")
+        
+        setUI()
     }
     
     override func didReceiveMemoryWarning() {
@@ -46,22 +48,56 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    deinit{
+        // Remove Observer
+        //NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
     override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
         if UIDevice.currentDevice().orientation.isLandscape.boolValue {
-            print("Landscape")
+            print("viewWillTransitionToSize Landscape")
         } else {
-            print("Portrait")
+            print("viewWillTransitionToSize Portrait")
         }
+     
+        coordinator.animateAlongsideTransition(nil, completion: {
+        
+            transitionCoordinatorContext  in
+            
+                self.setUI()
+        })
     }
+    
+
     
     //////////////////////////////////
     //      Private Func           //
     ////////////////////////////////
     
     private func initUI(){
+        // Add Observer Screen Rotated
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.screenRotated), name: UIDeviceOrientationDidChangeNotification, object: nil)
+    }
+    
+    private func setUI(){
+        // let the font size fit the Frame
         LabelTime.adjustFontSizeToFitRect(LabelTime.frame)
         ButtonReset.titleLabel!.adjustFontSizeToFitRect(ButtonReset.frame)
     }
+    
+//    @objc private func screenRotated()
+//    {
+//        if(UIDeviceOrientationIsLandscape(UIDevice.currentDevice().orientation))
+//        {
+//            print("rotated landscape")
+//        }
+//        
+//        if(UIDeviceOrientationIsPortrait(UIDevice.currentDevice().orientation))
+//        {
+//            print("rotated Portrait")
+//        }
+//        
+//    }
     
     //////////////////////////////////
     //      Public Func            //
