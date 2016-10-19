7cfb14989493ab3a7528d12b00d612238b58484c
SHUN <SHUN@shundeimac.calcomp.com.tw>
2016-10-19 16:31:17 +0800

    test

--- Project 04 - SnapChatMenu/Project 04 - SnapChatMenu/ViewController.swift
+++ Project 04 - SnapChatMenu/Project 04 - SnapChatMenu/ViewController.swift
@@ -9,17 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //==============================//
+    // MARK:     Pirvate Property
+    //=============================//
+    
+    
+    //==============================//
+    // MARK:     Public Property
+    //=============================//
+    
+    
+    
+    //==============================//
+    // MARK:     Layout Property
+    //=============================//
+    
+    @IBOutlet weak var segmented: UISegmentedControl!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //==============================//
+    // MARK:     Action
+    //=============================//
+    
+    @IBAction func segmentedValueChanged(sender: AnyObject) {
 
+        switch segmented.selectedSegmentIndex{
+        case 0:
+            /// Square
+            
+            imageView.layer.cornerRadius = 0
+            imageView.clipsToBounds = true
+            
+            break
+        case 1:
+            /// Circle
+            
+            imageView.layer.cornerRadius = imageView.frame.size.width / 2
+            imageView.clipsToBounds = true
+            
+            break
+        default:
+            break
+        }
+        
+    }
+    
+    
+    //==============================//
+    // MARK:     Life Cycle
+    //=============================//
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    //==============================//
+    // MARK:      Private Func
+    //=============================//
+    
+    
+    //==============================//
+    // MARK:      Public Func
+    //=============================//
+    
+    
+    
 }
 
