018e35c98423a6bab8d4019c36191e6eec25f00b
kinpomis <kinpomis@mis.calcomp.com.tw>
2016-10-21 08:50:55 +0800

    add project

---
+++ Project 6 - Get user's current location/Project 6 - Get user's current location/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 6 - Get user's current location
+//
+//  Created by kinpomis on 2016/10/21.
+//  Copyright © 2016年 q2650108. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
