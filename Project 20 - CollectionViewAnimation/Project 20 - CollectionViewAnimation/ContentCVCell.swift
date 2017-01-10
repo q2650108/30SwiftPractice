4e918fdce54c4d8665c6e5b111300d76f0544961
SHUN <SHUN@shun.local>
2017-01-10 20:19:12 +0800

    Fix Layout

---
+++ Project 20 - CollectionViewAnimation/Project 20 - CollectionViewAnimation/ContentCVCell.swift
@@ -0,0 +1,72 @@
+//
+//  ContentCVCell.swift
+//  Project 20 - CollectionViewAnimation
+//
+//  Created by SHUN on 1/10/17.
+//  Copyright © 2017 q2650108. All rights reserved.
+//
+
+import UIKit
+
+class ContentCVCell: UICollectionViewCell {
+   
+    
+    //==============================//
+    // MARK:     Pirvate Property
+    //=============================//
+    
+    
+    //==============================//
+    // MARK:     Public Property
+    //=============================//
+    
+    
+    
+    //==============================//
+    // MARK:     Layout Property
+    //=============================//
+    
+    @IBOutlet weak var imageViewContent: UIImageView!
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    
+    //==============================//
+    // MARK:     Action
+    //=============================//
+    
+    
+    //==============================//
+    // MARK:     Life Cycle
+    //=============================//
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    deinit{
+        print("\( type(of: self) ) deinit")
+        
+    }
+    
+    //==============================//
+    // MARK:      Private Func
+    //=============================//
+    
+    private func initValue(){
+        
+        
+    }
+    
+    //==============================//
+    // MARK:      Public Func
+    //=============================//
+    
+  
+    
+}
--- Project 20 - CollectionViewAnimation/Project 20 - CollectionViewAnimation/ContetnCVC.swift
+++ Project 20 - CollectionViewAnimation/Project 20 - CollectionViewAnimation/ContetnCVC.swift
@@ -8,87 +8,225 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
 
-class ContetnCVC: UICollectionViewController {
 
+class ContetnCVC: UICollectionViewController  {
+    
+    
+    //==============================//
+    // MARK:     Pirvate Property
+    //=============================//
+    
+    private let _margin : CGFloat = 10.0
+    
+    private let _spacing : CGFloat = 5.0
+    
+    private let _perCellNumm : CGFloat = 3.0
+    
+    private let reuseIdentifier = "cell"
+    
+    private let imageList = [ "image_1" , "image_2" , "image_3" , "image_4" , "image_5" , "image_6" ]
+    
+    private var _currentScrollDirection : UICollectionViewScrollDirection = .horizontal
+    
+    //==============================//
+    // MARK:     Public Property
+    //=============================//
+    
+    var margin : CGFloat {
+        get {
+            return _margin
+        }
+    }
+    
+    var spacing : CGFloat {
+        get {
+            return _spacing
+        }
+    }
+    
+    var perCellNumm : CGFloat {
+        get {
+            return _perCellNumm
+        }
+    }
+    
+    var currentScrollDirection : UICollectionViewScrollDirection {
+        get {
+            return _currentScrollDirection
+        }
+        set {
+            _currentScrollDirection = newValue
+        }
+    }
+    
+    //==============================//
+    // MARK:     Layout Property
+    //=============================//
+    
+    @IBOutlet weak var buttonChangeScrollDirection: UIBarButtonItem!
+    
+    
+    //==============================//
+    // MARK:     Action
+    //=============================//
+    
+    @IBAction func handleScrollDirection(_ sender: UIBarButtonItem) {
+        
+        if self.currentScrollDirection == .horizontal {
+            self.currentScrollDirection = .vertical
+            buttonChangeScrollDirection.title = "Horizontal"
+        }else {
+            self.currentScrollDirection = .horizontal
+            buttonChangeScrollDirection.title = "Vertical"
+        }
+        
+        let flowLayout = collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.scrollDirection = self.currentScrollDirection
+        
+        self.collectionView?.setCollectionViewLayout(flowLayout, animated: true)
+    }
+    
+    //==============================//
+    // MARK:     Life Cycle
+    //=============================//
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        initValue()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
+        //self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    deinit{
+        print("\( type(of: self) ) deinit")
+        
     }
-    */
-
-    // MARK: UICollectionViewDataSource
-
+    
+    //==============================//
+    // MARK:      Private Func
+    //=============================//
+    
+    private func initValue(){
+        self.collectionView?.delegate = self
+        
+        //                       let flow = collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        //                flow.scrollDirection
+        
+        
+    }
+    
+    
+    
+    
+    
+    //==============================//
+    // MARK:      Public Func
+    //=============================//
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameter collectionView: <#collectionView description#>
+    /// - Returns: <#return value description#>
+    func getMinSize( _ collectionView : UICollectionView ) -> CGSize {
+        var size : CGSize = CGSize()
+        if currentScrollDirection == .horizontal {
+            var minSide = ( collectionView.frame.height
+                - ( self.navigationController?.navigationBar.frame.height ?? 0 )
+                - UIApplication.shared.statusBarFrame.height
+                - margin * 2
+                - spacing * (perCellNumm - 1) )
+//            minSide =  minSide - (  spacing * 4 )
+            minSide =   minSide  / perCellNumm
+            size = CGSize(width: minSide , height: minSide  )
+        }else {
+            let minSide = ( collectionView.frame.width - margin * 2 - spacing * (perCellNumm - 1) ) / perCellNumm
+            size = CGSize(width: minSide , height: minSide )
+        }
+        return size
+        
+    }
+    
+    
+    //==============================//
+    // MARK:      UICollectionViewDataSource
+    //=============================//
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
-
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return imageList.count * 5
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ContentCVCell
+        
+        cell.imageViewContent.image = UIImage(named: imageList[indexPath.row % 5])
+        
+        cell.labelTitle.text = "Image_\(indexPath.row)"
+        
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    //==============================//
+    // MARK:      UICollectionViewDelegate
+    //=============================//
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        print( collectionView.cellForItem(at: indexPath)?.frame.height)
     }
-    */
+    
+}
 
+//==============================//
+// MARK:      UICollectionViewDelegateFlowLayout
+//=============================//
+
+extension ContetnCVC : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //        print("collectionView: \(collectionView)")
+        //        print("collectionViewLayout: \(collectionViewLayout)")
+        //        print("=======")
+        return getMinSize(collectionView)
+    }
+    
+    /// Margin
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        //print("insetForSectionAt")
+        
+        return UIEdgeInsets(top: margin , left: margin, bottom: margin, right: margin)
+    }
+    
+    /// For Line
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        print("minimumLineSpacingForSectionAt")
+        return spacing
+    }
+    
+    /// For Cell
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        print("minimumInteritemSpacingForSectionAt")
+        
+        return spacing
+    }
 }
