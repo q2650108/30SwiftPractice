61398e6a71468df0d7ced55de088dfc5ef00d4fa
Shun <SHUN@shun.calcomp.com.tw>
2017-01-25 15:14:54 +0800

    Add Project and GIF

---
+++ Project 27 - TabbarApp/Project 27 - TabbarApp/MyTBC.swift
@@ -0,0 +1,78 @@
+//
+//  MyTBC.swift
+//  Project 27 - TabbarApp
+//
+//  Created by SHUN on 1/25/17.
+//  Copyright © 2017 q2650108. All rights reserved.
+//
+
+import UIKit
+
+class MyTBC: UITabBarController {
+    
+    //==============================//
+    // MARK:     Pirvate Property
+    //=============================//
+    
+    
+    //==============================//
+    // MARK:     Public Property
+    //=============================//
+    
+    
+    
+    //==============================//
+    // MARK:     Layout Property
+    //=============================//
+    
+    //==============================//
+    // MARK:     Action
+    //=============================//
+    
+    
+    //==============================//
+    // MARK:     Life Cycle
+    //=============================//
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    deinit{
+        print("\( type(of: self) ) deinit")
+        
+    }
+    
+    //==============================//
+    // MARK:      Private Func
+    //=============================//
+    
+    private func initValue(){
+        
+        
+    }
+    
+    //==============================//
+    // MARK:      Public Func
+    //=============================//
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
